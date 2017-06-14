ccd02d2e3f12bc9d59c394d09ea70fedadcf9a79
Keerran <kumakx@kingschester.co.uk>
2017-06-14 14:37:29 +0100

    Add files via upload

--- Assets/Scripts/Base.cs
+++ Assets/Scripts/Base.cs
@@ -15,7 +15,7 @@ public class Base : MonoBehaviour
 	// Use this for initialization
 	void Start()
 	{
-		InvokeRepeating("Motion", 0, 1);
+		resetForce();
 	}
 	
 	// Update is called once per frame
@@ -27,9 +27,41 @@ public class Base : MonoBehaviour
 		vel.z += force.z * td / mass;
 
 		Vector3 pos = this.transform.position;
-		pos.x += vel.x * td;
+		Vector3 offset = vel * td;
+		/*pos.x += vel.x * td;
 		pos.y += vel.y * td;
-		pos.z += vel.z * td;
+		pos.z += vel.z * td;*/
+		RaycastHit hitInfo = new RaycastHit();
+		Physics.Raycast(pos, offset.normalized,offset.magnitude);
+
 		this.transform.position = pos;
+	}
+
+	public void resetForce()
+	{
+		force = new Vector3(0, -this.mass * Constants.g, 0);
+	}
+
+	public void addForce(Vector3 force)
+	{
+		/*
+		float xAngle = Vector3.Angle(Vector3.right, force);
+		print(xAngle);
+
+		float yAngle = Vector3.Angle(Vector3.up, force);
+		print(yAngle);
+
+		float zAngle = Vector3.Angle(Vector3.forward, force);
+		print(zAngle);*/
+		this.force += force;
+	}
+
+	public void addForce(int magnitude, Vector3 angle)
+	{
+		float x = magnitude * Mathf.Cos(angle.x);
+		float y = magnitude * Mathf.Cos(angle.y);
+		float z = magnitude * Mathf.Cos(angle.z);
+
+		this.force += new Vector3(x, y, z);
 	}
 }
---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Constants
+{
+	public static readonly float g = 9.80665f;
+
+}
